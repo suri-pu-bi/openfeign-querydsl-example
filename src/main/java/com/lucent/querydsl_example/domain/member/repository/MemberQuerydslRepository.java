e98604ced49bfd196b517dd5021eabdf6fee4b88
suri-pu-bi <ymg12347@gmail.com>
2024-08-07 22:23:02 +0900

    feat: 검색조건 추상메서드 생성

--- src/main/java/com/lucent/querydsl_example/domain/member/repository/MemberQuerydslRepository.java
+++ src/main/java/com/lucent/querydsl_example/domain/member/repository/MemberQuerydslRepository.java
@@ -1,4 +1,28 @@
 package com.lucent.querydsl_example.domain.member.repository;
 
+import java.util.List;
+
+import com.lucent.querydsl_example.domain.member.entity.Member;
+
 public interface MemberQuerydslRepository {
+	// 동일 여부
+	Member findEqualNameMember(String name);
+	List<Member> findNotEqualNameMember(String name);
+	List<Member> findNotNullNameMember();
+
+	// 포함 여부
+	List<Member> findAgeInMember(int minAge, int maxAge);
+	List<Member> findAgeNotInMember(int minAge, int maxAge);
+	List<Member> findAgeBetweenMember(int minAge, int maxAge);
+
+	// 문자열
+	List<Member> findLikeNameMember(String name);
+	List<Member> findStartsWithNameMember(String name);
+	Member findContainsNameFirstMember(String name);
+
+	// 수 비교
+	List<Member> findAgeGoeMember(int age);
+	List<Member> findAgeGtMember(int age);
+	List<Member> findAgeLoeMember(int age);
+	List<Member> findAgeLtMember(int age);
 }
