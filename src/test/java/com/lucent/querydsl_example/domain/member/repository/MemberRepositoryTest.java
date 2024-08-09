728d9d8d4e9c7189aa4e5a39510956e4e395bc81
suri-pu-bi <ymg12347@gmail.com>
2024-08-09 17:21:55 +0900

    test: memberRepository 조인 테스트 코드 작성

--- src/test/java/com/lucent/querydsl_example/domain/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/lucent/querydsl_example/domain/member/repository/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.lucent.querydsl_example.domain.member.repository;
 
+import static com.lucent.querydsl_example.domain.manager.entity.QManager.*;
 import static com.lucent.querydsl_example.domain.member.entity.QMember.*;
+import static com.lucent.querydsl_example.domain.team.entity.QTeam.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -17,7 +19,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import com.lucent.querydsl_example.domain.config.QuerydslTestConfig;
+import com.lucent.querydsl_example.domain.manager.entity.Manager;
+import com.lucent.querydsl_example.domain.manager.repository.ManagerRepository;
 import com.lucent.querydsl_example.domain.member.entity.Member;
+import com.lucent.querydsl_example.domain.team.entity.Team;
+import com.lucent.querydsl_example.domain.team.repository.TeamRepository;
 import com.querydsl.core.Tuple;
 
 @DataJpaTest
@@ -27,6 +33,10 @@ class MemberRepositoryTest {
 
 	@Autowired
 	private MemberRepository memberRepository;
+	@Autowired
+	private TeamRepository teamRepository;
+	@Autowired
+	private ManagerRepository managerRepository;
 
 	@BeforeEach
 	void setUp() {
@@ -66,26 +76,65 @@ class MemberRepositoryTest {
 		memberRepository.save(member4);
 		memberRepository.save(member5);
 
-		// Team team1 = Team.builder()
-		// 	.name("A1")
-		// 	.budget(70000000)
-		// 	.build();
-		//
-		// Team team2 = Team.builder()
-		// 	.name("A2")
-		// 	.budget(90000000)
-		// 	.build();
-		//
-		// Team team3 = Team.builder()
-		// 	.name("B1")
-		// 	.budget(100000000)
-		// 	.build();
-		//
-		// Team team4 = Team.builder()
-		// 	.name("B2")
-		// 	.budget(50000000)
-		// 	.build();
-		//
+		Team team1 = Team.builder()
+			.name("A1")
+			.budget(70000000)
+			.build();
+
+		Team team2 = Team.builder()
+			.name("A2")
+			.budget(90000000)
+			.build();
+
+		Team team3 = Team.builder()
+			.name("B1")
+			.budget(100000000)
+			.build();
+
+		Team team4 = Team.builder()
+			.name("B2")
+			.budget(50000000)
+			.build();
+
+		teamRepository.save(team1);
+		teamRepository.save(team2);
+		teamRepository.save(team3);
+		teamRepository.save(team4);
+
+		member1.updateTeam(team1);
+		member2.updateTeam(team1);
+		member3.updateTeam(team2);
+		member4.updateTeam(team3);
+		member5.updateTeam(team4);
+
+		Manager manager1 = Manager.builder()
+			.team(team1)
+			.name("매니저1")
+			.salary(70000000)
+			.build();
+
+		Manager manager2 = Manager.builder()
+			.team(team2)
+			.name("매니저2")
+			.salary(60000000)
+			.build();
+
+		Manager manager3 = Manager.builder()
+			.team(team3)
+			.name("매니저3")
+			.salary(50000000)
+			.build();
+
+		Manager manager4 = Manager.builder()
+			.team(team4)
+			.name("매니저4")
+			.salary(80000000)
+			.build();
+
+		managerRepository.save(manager1);
+		managerRepository.save(manager2);
+		managerRepository.save(manager3);
+		managerRepository.save(manager4);
 
 
 	}
@@ -222,7 +271,7 @@ class MemberRepositoryTest {
 	}
 
 	@Test
-	@DisplayName("회원 데이터의 집함 함수를 수행할 때, 카운트, 합계, 평균, 최대값, 최소값이 예상된 값과 일치한다")
+	@DisplayName("회원 데이터의 집함 함수를 수행할 때, 카운트가 5, 합계가 128, 평균이 25.6, 최대값이 30, 최소값이 23이다.")
 	public void aggregation() {
 		// when
 		Tuple result = memberRepository.aggregationMember();
@@ -236,7 +285,7 @@ class MemberRepositoryTest {
 	}
 
 	@Test
-	@DisplayName("회원의 나이를 내림차순으로 정렬하고, 나이가 같다면 이름으로 오름차순 정렬하며, 이름이 없는 회원은 맨 마지막에 올 때, 예상된 순서와 일치한다")
+	@DisplayName("회원의 나이를 내림차순으로 정렬하고, 나이가 같다면 이름으로 오름차순 정렬하며, 정렬된 결과가 '냥이', '주희', '수미', '동건', '몽미'와 일치한다")
 	public void sortMember() {
 		// when
 		List<Member> members =  memberRepository.sortMember();
@@ -250,7 +299,7 @@ class MemberRepositoryTest {
 	}
 
 	@Test
-	@DisplayName("멤버를 나이 내림차순, 이름 오름차순으로 정렬하고, 1번째부터 2개의 멤버를 페이징할 때, 예상된 순서와 일치한다")
+	@DisplayName("멤버를 나이 내림차순, 이름 오름차순으로 정렬하고, 1번째부터 2개의 멤버를 페이징할 때, 결과가 '주희', '수미'와 일치한다")
 	public void pagingMember() {
 		// when
 		List<Member> members = memberRepository.pagingMember();
@@ -262,7 +311,7 @@ class MemberRepositoryTest {
 	}
 
 	@Test
-	@DisplayName("회원 목록을 페이지 번호 1, 사이즈 2로 페이징할 때, 두 번째 페이지의 회원 목록이 예상과 일치하고, 마지막 페이지에서는 카운트 쿼리를 실행한다")
+	@DisplayName("회원 목록을 페이지 번호 1, 사이즈 2로 페이징할 때, 두 번째 페이지의 회원 목록이 '몽미'이고, 마지막 페이지에서는 카운트 쿼리를 실행한다")
 	public void pagingMemberUsingPageImpl() {
 		// given
 		// 페이지 번호가 1이고, 사이즈가 2이므로 페이지 번호 0: 냥이, 주희 / 1: 수미, 동건 / 2 : 몽미
@@ -276,7 +325,7 @@ class MemberRepositoryTest {
 	}
 
 	@Test
-	@DisplayName("회원 목록을 페이지 번호 1, 사이즈 2로 페이징할 때, 두 번째 페이지의 회원 목록이 예상과 일치하고, 마지막 페이지에서는 카운트 쿼리를 실행하지 않는다")
+	@DisplayName("회원 목록을 페이지 번호 1, 사이즈 2로 페이징할 때, 두 번째 페이지의 회원 목록이 '몽미'이고, 마지막 페이지에서는 카운트 쿼리를 실행하지 않는다")
 	public void pagingMemberUsingPageUtil() {
 		// given
 		// 페이지 번호가 1이고, 사이즈가 2이므로 페이지 번호 0: 냥이, 주희 / 1: 수미, 동건 / 2 : 몽미
@@ -289,6 +338,89 @@ class MemberRepositoryTest {
 		assertEquals(result.getContent().get(0).getName(), "몽미");
 	}
 
+	@Test
+	@DisplayName("팀 이름이 A1인 팀에 조인하여 해당 팀의 회원을 조회할 때, 회원 수가 2명이고 첫 번째 회원의 이름은 '수미'이다")
+	public void joinWhereTeamName(){
+		// given
+		String teamName = "A1";
+
+		// when
+		List<Member> result = memberRepository.joinWhereTeamName(teamName);
+
+		// then
+		assertEquals(result.size(), 2);
+		assertEquals(result.get(0).getName(), "수미");
+	}
+
+	@Test
+	@DisplayName("팀 이름이 A1인 팀에 대해 내부 조인하여 회원을 조회할 때, 회원 수가 2명이고 첫 번째 회원의 이름은 '수미'이다")
+	public void innerJoinWhereTeamName(){
+		// given
+		String teamName = "A1";
+
+		// when
+		List<Member> result = memberRepository.innerJoinWhereTeamName(teamName);
+
+		// then
+		assertEquals(result.size(), 2);
+		assertEquals(result.get(0).getName(), "수미");
+	}
+
+	@Test
+	@DisplayName("팀 이름이 A1인 팀에 대해 왼쪽 조인하여 회원을 조회할 때, 첫 번째 결과의 회원 이름이 '수미'이고 팀 이름이 'A1'이며, 세 번째 결과의 팀이 null이다")
+	public void leftJoinOnTeamName(){
+		// given
+		String teamName = "A1";
+
+		// when
+		List<Tuple> result = memberRepository.leftJoinOnTeamName(teamName);
+
+		// then
+		assertEquals(result.get(0).get(member).getName(), "수미");
+		assertEquals(result.get(0).get(team).getName(), "A1");
+		assertNull(result.get(2).get(team));
+	}
+
+	@Test
+	@DisplayName("팀 이름이 A1인 팀에 대해 오른쪽 조인하여 회원을 조회할 때, 첫 번째 결과의 회원 이름이 '수미'이고 팀 이름이 'A1'이며, 세 번째 결과의 팀은 null이 아니고 회원은 null이다")
+	public void rightJoinOnTeamName(){
+		// given
+		String teamName = "A1";
+
+		// when
+		List<Tuple> result = memberRepository.rightJoinOnTeamName(teamName);
+
+		// then
+		assertEquals(result.get(0).get(member).getName(), "수미");
+		assertEquals(result.get(0).get(team).getName(), "A1");
+		assertNotNull(result.get(2).get(team));
+		assertNull(result.get(2).get(member));
+	}
+
+	@Test
+	@DisplayName("세타 조인을 사용하여 회원 급여가 매니저 급여보다 낮은 경우를 조회할 때, 결과가 3개이며, 첫번째 회원의 이름은 동건이다.")
+
+	public void thetaJoinWhereMangerName(){
+		// when
+		List<Member> result = memberRepository.thetaJoinWhereMemberSalaryMoreThanManagerSalary();
+
+		// then
+		assertEquals(result.size(), 3);
+		assertEquals(result.get(0).getName(), "동건");
+	}
+
+	@Test
+	@DisplayName("연관관계 없는 엔티티끼리 왼쪽 조인을 사용하여 회원 급여가 매니저 급여보다 낮은 경우를 조회할 때, 결과가 8개이며, 8번째 결과의 매니저는 null이고, 세번쨰 매니저의 이름이 '매니저1', 네번째 매니저의 이름이 '매니저4'이다.")
+	public void lefJoinHaveNotRelationShipOnMemberSalaryMoreThanManagerSalary(){
+		// when
+		List<Tuple> result = memberRepository.lefJoinHaveNotRelationShipOnMemberSalaryMoreThanManagerSalary();
+
+		// then
+		assertEquals(result.size(), 8);
+		assertNull(result.get(7).get(manager));
+		assertEquals(result.get(2).get(manager).getName(), "매니저1");
+		assertEquals(result.get(3).get(manager).getName(), "매니저4");
+	}
 
 
 
