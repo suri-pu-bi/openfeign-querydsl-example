d5579df5dba5d1e273d10f9c659ed360e7282745
suri-pu-bi <ymg12347@gmail.com>
2024-08-12 17:44:46 +0900

    feat: teamRepository 프로젝션 추상 메서드 생성

--- src/main/java/com/lucent/querydsl_example/domain/team/repository/TeamQuerydslRepository.java
+++ src/main/java/com/lucent/querydsl_example/domain/team/repository/TeamQuerydslRepository.java
@@ -1,4 +1,10 @@
 package com.lucent.querydsl_example.domain.team.repository;
 
+import java.util.List;
+
+import com.lucent.querydsl_example.domain.team.dto.TeamResponse;
+
 public interface TeamQuerydslRepository {
+	// 프로젝션
+	List<TeamResponse> projectionTeam();
 }
