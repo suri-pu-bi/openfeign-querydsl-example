86e840ffa814ffae4932915b02b5b85d95e35644
suri-pu-bi <ymg12347@gmail.com>
2024-08-13 15:24:50 +0900

    feat: fetchJoin에서 연관관계 다쪽에 where 필터링을 하는 추상메서드 생성

--- src/main/java/com/lucent/querydsl_example/domain/team/repository/TeamQuerydslRepository.java
+++ src/main/java/com/lucent/querydsl_example/domain/team/repository/TeamQuerydslRepository.java
@@ -2,9 +2,14 @@ package com.lucent.querydsl_example.domain.team.repository;
 
 import java.util.List;
 
+
 import com.lucent.querydsl_example.domain.team.dto.TeamResponse;
+import com.lucent.querydsl_example.domain.team.entity.Team;
 
 public interface TeamQuerydslRepository {
 	// 프로젝션
 	List<TeamResponse> projectionTeam();
+
+	// fetchJoin with Where - Error
+	List<Team> fetchJoinWithWhere();
 }
