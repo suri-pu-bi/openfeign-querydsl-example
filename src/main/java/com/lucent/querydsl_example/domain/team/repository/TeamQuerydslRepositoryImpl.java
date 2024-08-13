35c482b85cb4aad76cd0b4d0a0f6add78e97ae2e
suri-pu-bi <ymg12347@gmail.com>
2024-08-13 15:25:06 +0900

    feat: fetchJoin에서 연관관계 다쪽에 where 필터링을 하는 추상메서드 구현

--- src/main/java/com/lucent/querydsl_example/domain/team/repository/TeamQuerydslRepositoryImpl.java
+++ src/main/java/com/lucent/querydsl_example/domain/team/repository/TeamQuerydslRepositoryImpl.java
@@ -1,11 +1,13 @@
 package com.lucent.querydsl_example.domain.team.repository;
 
+import static com.lucent.querydsl_example.domain.member.entity.QMember.*;
 import static com.lucent.querydsl_example.domain.team.entity.QTeam.*;
 
 import java.util.List;
 
 import com.lucent.querydsl_example.domain.team.dto.QTeamResponse;
 import com.lucent.querydsl_example.domain.team.dto.TeamResponse;
+import com.lucent.querydsl_example.domain.team.entity.Team;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -21,6 +23,15 @@ public class TeamQuerydslRepositoryImpl implements TeamQuerydslRepository {
 				new QTeamResponse(
 					team.name.as("teamName"), team.budget))
 			.from(team)
+			.fetch();
+	}
+
+	@Override
+	public List<Team> fetchJoinWithWhere() {
+		return queryFactory
+			.selectFrom(team)
+			.join(team.members, member).fetchJoin()
+			.where(member.age.goe(23))
 			.fetch();
 	}
 }
